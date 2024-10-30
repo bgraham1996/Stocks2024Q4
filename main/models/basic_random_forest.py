6746a6386ca798691ccaeea1271e272f430546e6
Ben G <benlauriegraham@gmail.com>
2024-10-30 18:39:44 +0000

    readded period iterator

--- main/models/basic_random_forest.py
+++ main/models/basic_random_forest.py
@@ -85,3 +85,21 @@ def basic_rf(data, target='60_return', features=['RSI_Signal', 'SMA_Signal', 'EM
         preds.append(y_pred)
     
     return forrest, feature_importances, preds
+
+
+def period_iterator(data, periods = [
+    '5_return', '10_return', '15_return', '20_return', '25_return', '30_return', '40_return', '50_return', '60_return'], debug=True):
+    models_dict = {}
+    preds_dict = {}
+    fi_dict = {}
+    
+    for period in periods:
+        print('===========================================')
+        print(f'Running for period: {period}')
+        model, fi, preds = basic_rf(data, target=period, debug=debug)
+        key = period
+        models_dict[key] = model
+        fi_dict[key] = fi
+        preds_dict[key] = preds
+    
+    return models_dict, fi_dict, preds_dict
